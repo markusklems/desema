ec868752cfffc78924d427251d30c36c266771f0
Christian Kniep <christian@qnib.org>
2016-12-03 12:36:49 +0100

    adjust startscript dockerfile and compose file

--- workbench/poc_angular2_ipfs_ethereum/docker/geth/start.sh
+++ workbench/poc_angular2_ipfs_ethereum/docker/geth/start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-#geth --rpc --rpccorsdomain "http://0.0.0.0:4200"
-geth --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcapi eth0,lo
+geth --rpc --rpccorsdomain "http://localhost:4200"
